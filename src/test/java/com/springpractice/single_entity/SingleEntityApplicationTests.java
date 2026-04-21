62e00a003c26c2f5644d3641084f62827335e96f
jayashree <jayashreefour20@gmail.com>
2026-04-21 10:48:19 +0530

    Intialcommit

---
+++ src/test/java/com/springpractice/single_entity/SingleEntityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springpractice.single_entity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingleEntityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
